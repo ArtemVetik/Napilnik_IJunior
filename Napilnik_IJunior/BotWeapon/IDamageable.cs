f7bca331bd26671eeeb3b3af84ec9c8ad20dc6f9
Artem <av.unitygames@gmail.com>
2021-11-24 12:29:59 +0300

    Complete task 1

---
+++ Napilnik_IJunior/BotWeapon/IDamageable.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Napilnik.BotWeapon
+{
+    interface IDamageable
+    {
+        void Damage(int value);
+    }
+}
---
+++ Napilnik_IJunior/BotWeapon/Player.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Napilnik.BotWeapon
+{
+    class Player : IDamageable
+    {
+        public Player(int health)
+        {
+            if (health < 0)
+                throw new ArgumentOutOfRangeException(nameof(health));
+
+            Health = health;
+        }
+
+        public int Health { get; private set; }
+        public bool Dead => Health == 0;
+
+        public void Damage(int value)
+        {
+            if (Dead)
+                throw new InvalidOperationException();
+            if (value < 0)
+                throw new ArgumentOutOfRangeException(nameof(value));
+
+            Health -= value;
+
+            if (Health < 0)
+                Health = 0;
+        }
+    }
+}
